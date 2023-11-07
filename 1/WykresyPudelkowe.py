a1275748ef2f31ff92a6b7a03a5df9b28b748b4f
247644 Dominiak Maciej <247644@edu.p.lodz.pl>
2023-11-07 13:58:57 +0100

    Boxowe Wyrkesy Dodane, nawet się rysują

---
+++ 1/WykresyPudelkowe.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from FunkcjeLiczace import *
+
+
+class WykresyPudelkowe:
+
+    def narysujBoxplot(dana, pion, Nazwa):
+        kolumna1 = FunkcjeLiczace.dajKolumne(dana, pion)
+        kolumna2 = FunkcjeLiczace.dajKolumne(dana, 4)
+
+        gatunku0 = []
+        gatunku1 = []
+        gatunku2 = []
+
+        for i in range(0, 150, 1):
+            if (kolumna2[i] == 0):
+                gatunku0.append(kolumna1[i])
+            if (kolumna2[i] == 1):
+                gatunku1.append(kolumna1[i])
+            if (kolumna2[i] == 2):
+                gatunku2.append(kolumna1[i])
+
+        data = [gatunku0,gatunku1,gatunku2]
+        fig = plt.figure(figsize=(5, 5))
+        ax = fig.add_axes([0.12, 0.12, 0.82, 0.82])
+
+        bp = ax.boxplot(data)
+
+        plt.title(Nazwa)
+
+        plt.show()
