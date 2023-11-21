4f991e099a1e6e90ccbc6c7169f48073c66d77f5
247638 Centkowski Łukasz <247638@edu.p.lodz.pl>
2023-11-21 13:04:04 +0100

    wyznaczona wartość współczynnika korelacji liniowej Pearsona oraz wyznaczone równanie regresji liniowej

---
+++ 2/WykresyPunktowe.py
@@ -0,0 +1,51 @@
+from FunkcjeLiczace import *
+import matplotlib.pyplot as plt
+
+class WykresyPunktowe:
+    def policzPudła(dane, pion_x, pion_y, xlabel, ylabel):
+
+        dane_z_pion_x, dane_z_pion_y, wynik, x_av, x_odch, y_av, y_odch  = WykresyPunktowe.liczymy_r(dane, pion_x, pion_y)
+
+        #rownanie regresji
+        x_min, x_max = FunkcjeLiczace.szukajMinimum(dane, pion_x), FunkcjeLiczace.szukajMaksimum(dane, pion_x)
+        y_min, y_max = FunkcjeLiczace.szukajMinimum(dane, pion_y), FunkcjeLiczace.szukajMaksimum(dane, pion_y)
+        a = wynik * (y_odch / x_odch)
+        b = y_av - a * x_av
+
+        WykresyPunktowe.narysujPudła(dane_z_pion_x, dane_z_pion_y, x_min, x_max, y_min, y_max, a, b, wynik, xlabel, ylabel)
+
+    def narysujPudła(pion_x, pion_y, x_min, x_max, y_min, y_max, a, b, wynik, xlabel, ylabel):
+
+        plt.scatter(pion_x, pion_y, c=pion_x, cmap='viridis')
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.plot([x_min, x_max], [a * x_min + b, a * x_max + b], color='blue', label=f"y = {round(a, 1)}x + {round(b, 1)}")
+        plt.title(f"r = {wynik:.2f}; y = {round(a, 1)}x + {round(b, 1)}")
+        plt.xticks()
+
+
+        plt.xlim(x_min-0.4, x_max+0.2)
+        plt.ylim(y_min-0.2, y_max+0.2)
+
+        plt.show()
+
+
+    def liczymy_r(dane, pion_x, pion_y):
+        n = FunkcjeLiczace.sizeOf(dane)
+
+        x_av, x_odch = FunkcjeLiczace.liczSredniaArytmetyczna(dane, 0)
+        y_av, y_odch = FunkcjeLiczace.liczSredniaArytmetyczna(dane, 1)
+
+        sum = 0
+        for wiersz in dane:
+            lewy = wiersz[pion_x] * wiersz[pion_y]
+            prawy = (x_av * y_av)
+            sum += lewy - prawy
+        sum_n = sum / n
+
+        dane_z_pion_x = FunkcjeLiczace.dajKolumne(dane, pion_x)
+        dane_z_pion_y = FunkcjeLiczace.dajKolumne(dane, pion_y)
+
+        wynik = sum_n / (x_odch * y_odch)
+        return dane_z_pion_x, dane_z_pion_y, wynik, x_av, x_odch, y_av, y_odch
+
--- 2/main2.py
+++ 2/main2.py
@@ -1,6 +1,5 @@
 import csv
-from FunkcjeLiczace import *
-import matplotlib.pyplot as plt
+from WykresyPunktowe import *
 dane = []
 
 with open("./data.csv", newline='') as plik_csv:
@@ -9,26 +8,9 @@ with open("./data.csv", newline='') as plik_csv:
         wiersz_float = [float(wartość) for wartość in wiersz]
         dane.append(wiersz_float)
 
-n = FunkcjeLiczace.sizeOf(dane)
-
-x_av,x_odch = FunkcjeLiczace.liczSredniaArytmetyczna(dane, 0)
-y_av,y_odch = FunkcjeLiczace.liczSredniaArytmetyczna(dane, 1)
-
-sum = 0
-for wiersz in dane:
-    lewy = wiersz[0] * wiersz[1]
-    prawy = (x_av * y_av)
-    sum += lewy - prawy
-sum_n = sum / n
-
-pion_x = FunkcjeLiczace.dajKolumne(dane,0)
-pion_y = FunkcjeLiczace.dajKolumne(dane,1)
-
-wynik = sum_n / (x_odch * y_odch)
-
-plt.scatter(pion_x,pion_y, c=pion_x)
-plt.plot([169, 189], [7.23, 11.48])
-#plt.xlim(0, 1)
-
-plt.show()
-print(wynik)
+    WykresyPunktowe.policzPudła(dane, 0, 1, "Długość działki kielicha (cm)", "Szerokość działki kielicha (cm)")
+    WykresyPunktowe.policzPudła(dane, 0, 2, "Długość działki kielicha (cm)", "Długość płatka (cm)")
+    WykresyPunktowe.policzPudła(dane, 0, 3, "Długość działki kielicha (cm)", "Szerokość płatka (cm)")
+    WykresyPunktowe.policzPudła(dane, 1, 2, "Szerokość działki kielicha (cm)", "Długość płatka (cm)")
+    WykresyPunktowe.policzPudła(dane, 1, 3, "Szerokość działki kielicha (cm)", "Szerokość płatka (cm)")
+    WykresyPunktowe.policzPudła(dane, 2, 3, "Długość płata (cm)", "Szerokość płatka (cm)")
