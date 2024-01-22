eec5089d5d282bc3cda52570e91bb0062d99f4de
247644 Dominiak Maciej <247644@edu.p.lodz.pl>
2024-01-22 23:38:45 +0100

    Witam, oddajemy?

--- 3/knn.py
+++ 3/knn.py
@@ -1,64 +1,64 @@
-def normalizacja(minimum, maksimum, argument):
+def scaling(minimum, maksimum, argument):
     return (argument - minimum) / (maksimum - minimum) #normalizacja min max dla zakresu <0;1>
 
 
-def odleglosc_dla_dwoch(x1, x2, y1, y2):
+def distance_for_two(x1, x2, y1, y2):
     return (((x1 - x2) ** 2) + ((y1 - y2) ** 2)) ** 0.5
 
 
-def odleglosc_dla_cztererch(x1, x2, y1, y2, z1, z2, v1, v2):
+def distance_for_four(x1, x2, y1, y2, z1, z2, v1, v2):
     return (((x1 - x2) ** 2) + ((y1 - y2) ** 2) + ((z1 - z2) ** 2) + ((v1 - v2) ** 2)) ** 0.5
 
 
-def lista_odleglosci_dla_dwoch(trening_1, testowa_1, trening_2, testowa_2):
-    lista_odleglosci = []
+def distance_list_for_two(trening_1, test_1, trening_2, test_2):
+    list_of_distances = []
     for i in range(len(trening_1)):
-        lista_odleglosci.append(odleglosc_dla_dwoch(trening_1[i], testowa_1, trening_2[i], testowa_2))
-    return lista_odleglosci
+        list_of_distances.append(distance_for_two(trening_1[i], test_1, trening_2[i], test_2))
+    return list_of_distances
 
 
-def lista_odleglosci_dla_czterech(trening_1, testowa_1, trening_2, testowa_2, trening_3, testowa_3, trening_4,
-                                  testowa_4):
+def distance_list_for_four(trening_1, test_1, trening_2, test_2, trening_3, test_3, trening_4,
+                           test_4):
     lista_odleglosci = []
     for i in range(len(trening_1)):
         lista_odleglosci.append(
-            odleglosc_dla_cztererch(trening_1[i], testowa_1, trening_2[i], testowa_2, trening_3[i], testowa_3,
-                                    trening_4[i], testowa_4))
+            distance_for_four(trening_1[i], test_1, trening_2[i], test_2, trening_3[i], test_3,
+                              trening_4[i], test_4))
     return lista_odleglosci
 
 
-def sprawdz_ile_gatunkow(lista): #sprawdzenie ile jest roznych gatunkow
-    lista_ile_roznych = []
+def check_species_number(lista): #sprawdzenie ile jest roznych gatunkow
+    list_of_num_differences = []
     for i in lista:
-        if i not in lista_ile_roznych:
-            lista_ile_roznych.append(i)
-    return len(lista_ile_roznych)
+        if i not in list_of_num_differences:
+            list_of_num_differences.append(i)
+    return len(list_of_num_differences)
 
 
-def knn(lista_odleglosci, lista_gatunkow, k):
-    indeksy_o_najmniejszych_odleglosciach = []
-    ilosc_dla_gatunku = []
-    for i in range(sprawdz_ile_gatunkow(lista_gatunkow)): #wypełnienie listy ile jest możliwych gatunków
-        ilosc_dla_gatunku.append(0)
+def knn(list_of_distances, list_of_species, k):
+    indexes_that_are_closest = []
+    num_for_species = []
+    for i in range(check_species_number(list_of_species)): #wypełnienie listy ile jest możliwych gatunków
+        num_for_species.append(0)
     for i in range(k):                                    #wypełnienie tablicy tak, żeby było k elementów, działamy na indeksach, które mają odniesienie do listy odległości
-        indeksy_o_najmniejszych_odleglosciach.append(i)
+        indexes_that_are_closest.append(i)
     for i in range(k - 1):
         for j in range(k - i - 1):
-            if lista_odleglosci[indeksy_o_najmniejszych_odleglosciach[j]] > lista_odleglosci[   #sortowanie tablicy od najmniejszej wartości odległości
-                indeksy_o_najmniejszych_odleglosciach[j + 1]]:
-                var = indeksy_o_najmniejszych_odleglosciach[j]
-                indeksy_o_najmniejszych_odleglosciach[j] = indeksy_o_najmniejszych_odleglosciach[j + 1]
-                indeksy_o_najmniejszych_odleglosciach[j + 1] = var
-    for i in range(k, len(lista_odleglosci)):
+            if list_of_distances[indexes_that_are_closest[j]] > list_of_distances[   #sortowanie tablicy od najmniejszej wartości odległości
+                indexes_that_are_closest[j + 1]]:
+                var = indexes_that_are_closest[j]
+                indexes_that_are_closest[j] = indexes_that_are_closest[j + 1]
+                indexes_that_are_closest[j + 1] = var
+    for i in range(k, len(list_of_distances)):
         for j in range(k):
-            if lista_odleglosci[i] < lista_odleglosci[indeksy_o_najmniejszych_odleglosciach[j]]:
+            if list_of_distances[i] < list_of_distances[indexes_that_are_closest[j]]:
                 for l in range(k - 1, j, -1):
-                    indeksy_o_najmniejszych_odleglosciach[l] = indeksy_o_najmniejszych_odleglosciach[l - 1]
-                indeksy_o_najmniejszych_odleglosciach[j] = i                                                    #sprawdzenie, czy badany element jest mniejszy od któregoś z elementów tablicy indeksów, jeśli tak to przesuwamy wszystkie elementy większe o jeden w prawo (oprócz ostatniego, który znika z tablicy) i wkładamy w odpiowiednie miejsce nr indeksowy tego elementu
+                    indexes_that_are_closest[l] = indexes_that_are_closest[l - 1]
+                indexes_that_are_closest[j] = i                                                    #sprawdzenie, czy badany element jest mniejszy od któregoś z elementów tablicy indeksów, jeśli tak to przesuwamy wszystkie elementy większe o jeden w prawo (oprócz ostatniego, który znika z tablicy) i wkładamy w odpiowiednie miejsce nr indeksowy tego elementu
                 break
     for i in range(k):
-        ilosc_dla_gatunku[lista_gatunkow[indeksy_o_najmniejszych_odleglosciach[i]]] += 1 #podliczamy, z jakiej klasy są elementy o najmniejszych odległościach
-    if ilosc_dla_gatunku.count(max(ilosc_dla_gatunku)) == 1: #jeśli mamy remis, to sprawdzamy knn dla k o jeden mniejszego, w przeciwnym wypadku zwracamy nr odpowiedniego gatunku
-        return ilosc_dla_gatunku.index(max(ilosc_dla_gatunku))
+        num_for_species[list_of_species[indexes_that_are_closest[i]]] += 1 #podliczamy, z jakiej klasy są elementy o najmniejszych odległościach
+    if num_for_species.count(max(num_for_species)) == 1: #jeśli mamy remis, to sprawdzamy knn dla k o jeden mniejszego, w przeciwnym wypadku zwracamy nr odpowiedniego gatunku
+        return num_for_species.index(max(num_for_species))
     else:
-        return knn(lista_odleglosci, lista_gatunkow, k - 1)
+        return knn(list_of_distances, list_of_species, k - 1)
--- 3/main.py
+++ 3/main.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot as plt
 import kmeans
 import seaborn as sns
 import knn
+
+
 def switch_example(attr1):
     switch_dict = {
         0: "Długość działki kielicha (cm)",
@@ -14,12 +16,14 @@ def switch_example(attr1):
     result = switch_dict.get(attr1)
     return result
 
+
 def display_iteration(iteration_list, k_list):
-    sns.barplot(x=k_list, y=iteration_list, color='blue')
+    sns.barplot(x=k_list, y=iteration_list, color='pink')
     plt.title('Liczba iteracji vs Liczba klastrów')
     plt.xlabel('Liczba klastrów')
     plt.ylabel('Liczba iteracji')
     plt.show()
+
 
 def display_WCSS(wcss_list, k_list):
     sns.lineplot(x=k_list, y=wcss_list, marker='o')
@@ -32,17 +36,20 @@ def display_WCSS(wcss_list, k_list):
 def display(centroids, clusters, attr1, attr2, title):
     plt.figure(figsize=(10, 8))
 
-    cluster_labels = ['Cluster 1', 'Cluster 2', 'Cluster 3']
+    cluster_labels = ['Cluster 1', 'Cluster 2', 'Cluster 3', 'Cluster 4', 'Cluster 5', 'Cluster 6', 'Cluster 7',
+                      'Cluster 8']
 
     for i, cluster in enumerate(clusters):
         if len(cluster) > 0:
             ctv_cluster = list(zip(*cluster))
             sns.scatterplot(x=ctv_cluster[attr1], y=ctv_cluster[attr2], label=cluster_labels[i],
-                            marker='o', facecolors='none', edgecolor=['green', 'red', 'blue'][i])
+                            marker='o', facecolors='none',
+                            edgecolor=['green', 'red', 'blue', 'yellow', 'brown', 'cyan', 'pink', 'purple'][i])
 
     for i, centroid in enumerate(centroids):
         sns.scatterplot(x=[centroid[attr1]], y=[centroid[attr2]], label=f'Centroid from {cluster_labels[i]}',
-                        marker='X', s=100, color=['green', 'red', 'blue'][i])
+                        marker='X', s=100,
+                        color=['green', 'red', 'blue', 'yellow', 'brown', 'cyan', 'pink', 'purple'][i])
 
     plt.xlabel(switch_example(attr1))
     plt.ylabel(switch_example(attr2))
@@ -61,7 +68,8 @@ data = pd.read_csv('data.csv', names=column_names)
 
 data = list(zip(*[data[column_names[col]].tolist() for col in range(4)]))
 
-centroids, clusters, iteration = kmeans.kMeans(data, 3, 100)
+centroids, clusters, iteration = kmeans.kMeans(data, 3, 1000)
+# maksymalnie k to 6, wynika to tylko z tego że trzeba jawnie kolor napisać.
 
 
 atributes_combinations = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
@@ -73,7 +81,7 @@ for attr1, attr2 in atributes_combinations:
 wcss_list = []
 iteration_list = []
 k_list = []
-for i in range(2,11):
+for i in range(2, 11):
     centroids, clusters, iteration = kmeans.kMeans(data, i, 100)
     wcss_list.append(kmeans.calculateWCSS(centroids, clusters))
     iteration_list.append(iteration)
@@ -82,152 +90,156 @@ for i in range(2,11):
 display_WCSS(wcss_list, k_list)
 display_iteration(iteration_list, k_list)
 
- ##knn
 
-def czyszczenie_output():
+##knn
+
+def output_clear():
     for i in range(50):
         print(" ")
-
-def czyszczenie_macierzy_pomylek():
-    for i in range(ile_gatunkow):
-        for j in range(ile_gatunkow):
-            macierz_pomylek[i][j] = 0
+        # Metoda po prostu rysuje pustkę na konsoli
 
 
-def procent_sukcesu():
-    ile_udanych = 0
-    for i in range(ile_gatunkow):
-        ile_udanych += macierz_pomylek[i][i]
-    return (round((ile_udanych) / (len(lista_testowa[i])) * 100, 1))
+def clear_of_error_matrix():
+    for i in range(species_number):
+        for j in range(species_number):
+            error_matrix[i][j] = 0
+    # Czyścimy macierz z błędami
 
 
-def wybor_najlepsze_k(lista_sukces_procent):
-    lista_ciagow = []
-    lista_aktualnego_ciagu = []
-    indeks_ciagu = 0
-    maksymalna_dlugosc_indeks = 0
+def success_percentage():
+    numbof_succeeded = 0
+    for i in range(species_number):
+        numbof_succeeded += error_matrix[i][i]
+    return (round((numbof_succeeded) / (len(test_list[i])) * 100, 1))
+    # Liczymy procent udanych dopasowań
+
+
+def choose_best_k(list_success_percentage):
+    sequence_list = []
+    list_of_current_sequence = []
+    index_of_sequence = 0
+    max_length_index = 0
     var = 1
-    for i in lista_sukces_procent:
-        if i == max(lista_sukces_procent):
-            lista_aktualnego_ciagu.append(i)
+    for i in list_success_percentage:
+        if i == max(list_success_percentage):
+            list_of_current_sequence.append(i)
         else:
-
-            lista_ciagow.append([indeks_ciagu, lista_aktualnego_ciagu.copy()])
-            lista_aktualnego_ciagu = []
-            indeks_ciagu = var
+            sequence_list.append([index_of_sequence, list_of_current_sequence.copy()])
+            list_of_current_sequence = []
+            index_of_sequence = var
         var += 1
 
-    lista_ciagow.append([indeks_ciagu, lista_aktualnego_ciagu.copy()])
-    for i in lista_ciagow:
-        if len(i[1]) > len(lista_ciagow[maksymalna_dlugosc_indeks][1]):
-            maksymalna_dlugosc_indeks = lista_ciagow.index(i)
-    return lista_ciagow[maksymalna_dlugosc_indeks][0] + 1
+    sequence_list.append([index_of_sequence, list_of_current_sequence.copy()])
+    for i in sequence_list:
+        if len(i[1]) > len(sequence_list[max_length_index][1]):
+            max_length_index = sequence_list.index(i)
+    return sequence_list[max_length_index][0] + 1
 
 
-def rysuj_tabelke(k, parametr1, parametr2,  czyDlaCzterech):
-    listy_z_wynikami_kolumna = []
-    listy_z_wynikami_wiersze = []
-    for i in range(ile_gatunkow):
-        for j in range(ile_gatunkow):
-            listy_z_wynikami_kolumna.append(macierz_pomylek[i][j])
-        listy_z_wynikami_wiersze.append(listy_z_wynikami_kolumna.copy())
-        listy_z_wynikami_kolumna = []
+def draw_table(k, parametr1, parametr2, isForFour):
+    lists_with_outputs_columns = []
+    lists_with_outputs_rows = []
+    for i in range(species_number):
+        for j in range(species_number):
+            lists_with_outputs_columns.append(error_matrix[i][j])
+        lists_with_outputs_rows.append(lists_with_outputs_columns.copy())
+        lists_with_outputs_columns = []
 
-    tabelka_do_wyswietlenia = {
+    tab_to_show = {
         "k = " + str(k) + "     Rozpoznanie --->\n   Faktyczna klasa v ": ["Setosa", "Versicolor", "Virginica"],
-        "Setosa": listy_z_wynikami_wiersze[0], "Versicolor": listy_z_wynikami_wiersze[1],
-        "Virginica": listy_z_wynikami_wiersze[2]}
-    if czyDlaCzterech:
+        "Setosa": lists_with_outputs_rows[0], "Versicolor": lists_with_outputs_rows[1],
+        "Virginica": lists_with_outputs_rows[2]}
+    if isForFour:
         print("Dla czterech parametrow")
     else:
-        print(nazwy_kolumn[parametr1] + " i " + nazwy_kolumn[parametr2])
-    print(tabulate.tabulate(tabelka_do_wyswietlenia, headers="keys", tablefmt="fancy_grid", showindex=False))
-    czyszczenie_macierzy_pomylek()
+        print(coll_name[parametr1] + " i " + coll_name[parametr2])
+    print(tabulate.tabulate(tab_to_show, headers="keys", tablefmt="fancy_grid", showindex=False))
+    clear_of_error_matrix()
 
-def rysuj(czy_dla_czterech, parametr1, parametr2):
-    lista_sukces_procent = []
-    if czy_dla_czterech:
+
+def draw(is_for_four, parametr1, parametr2, title):
+    success_percentage_list = []
+    if is_for_four:
         for k in range(1, 16):  # knn dla czterzech parametrow
-            for i in range(len(lista_testowa[0])):
-                wynik = knn.knn(
-                    knn.lista_odleglosci_dla_czterech(lista_treningowa[0], lista_testowa[0][i], lista_treningowa[1],
-                                                      lista_testowa[1][i], lista_treningowa[2], lista_testowa[2][i],
-                                                      lista_treningowa[3], lista_testowa[3][i]), lista_treningowa[4], k)
-                macierz_pomylek[lista_testowa[4][i]][wynik] += 1
-            lista_sukces_procent.append(procent_sukcesu())
-            czyszczenie_macierzy_pomylek()
-        najlepsze_k = wybor_najlepsze_k(lista_sukces_procent)
-        for i in range(len(lista_testowa[0])):
-            wynik = knn.knn(
-                knn.lista_odleglosci_dla_czterech(lista_treningowa[0], lista_testowa[0][i], lista_treningowa[1],
-                                                  lista_testowa[1][i], lista_treningowa[2], lista_testowa[2][i],
-                                                  lista_treningowa[3], lista_testowa[3][i]), lista_treningowa[4],
-                najlepsze_k)
-            macierz_pomylek[lista_testowa[4][i]][wynik] += 1
+            for i in range(len(test_list[0])):
+                result = knn.knn(
+                    knn.distance_list_for_four(train_list[0], test_list[0][i], train_list[1],
+                                               test_list[1][i], train_list[2], test_list[2][i],
+                                               train_list[3], test_list[3][i]), train_list[4], k)
+                error_matrix[test_list[4][i]][result] += 1
+            success_percentage_list.append(success_percentage())
+            clear_of_error_matrix()
+        best_k = choose_best_k(success_percentage_list)
+        for i in range(len(test_list[0])):
+            result = knn.knn(
+                knn.distance_list_for_four(train_list[0], test_list[0][i], train_list[1],
+                                           test_list[1][i], train_list[2], test_list[2][i],
+                                           train_list[3], test_list[3][i]), train_list[4],
+                best_k)
+            error_matrix[test_list[4][i]][result] += 1
     else:
         for k in range(1, 16):  # knn dla dwoch parametrow
-            for i in range(len(lista_testowa[0])):
-                wynik = knn.knn(
-                    knn.lista_odleglosci_dla_dwoch(lista_treningowa[parametr1], lista_testowa[parametr1][i],
-                                                   lista_treningowa[parametr2], lista_testowa[parametr2][i]),
-                    lista_treningowa[4], k)
-                macierz_pomylek[lista_testowa[4][i]][wynik] += 1
-            lista_sukces_procent.append(procent_sukcesu())
-            czyszczenie_macierzy_pomylek()
-        najlepsze_k = wybor_najlepsze_k(lista_sukces_procent)
-        for i in range(len(lista_testowa[0])):
-            wynik = knn.knn(
-                knn.lista_odleglosci_dla_dwoch(lista_treningowa[parametr1], lista_testowa[parametr1][i],
-                                               lista_treningowa[parametr2], lista_testowa[parametr2][i]),
-                lista_treningowa[4], najlepsze_k)
-            macierz_pomylek[lista_testowa[4][i]][wynik] += 1
+            for i in range(len(test_list[0])):
+                result = knn.knn(
+                    knn.distance_list_for_two(train_list[parametr1], test_list[parametr1][i],
+                                              train_list[parametr2], test_list[parametr2][i]),
+                    train_list[4], k)
+                error_matrix[test_list[4][i]][result] += 1
+            success_percentage_list.append(success_percentage())
+            clear_of_error_matrix()
+        best_k = choose_best_k(success_percentage_list)
+        for i in range(len(test_list[0])):
+            result = knn.knn(
+                knn.distance_list_for_two(train_list[parametr1], test_list[parametr1][i],
+                                          train_list[parametr2], test_list[parametr2][i]),
+                train_list[4], best_k)
+            error_matrix[test_list[4][i]][result] += 1
 
-    plt.plot(range(1, 16), lista_sukces_procent)
+    plt.plot(range(1, 16), success_percentage_list)
     plt.xlabel('k')
     plt.ylabel('Wynik [%]')
-    rysuj_tabelke(najlepsze_k, parametr1, parametr2,czy_dla_czterech)
+    plt.title(title)
+    draw_table(best_k, parametr1, parametr2, is_for_four)
     plt.show()
-    czyszczenie_macierzy_pomylek()
-    czyszczenie_output()
+    clear_of_error_matrix()
+    output_clear()
 
-nazwy_kolumn = ['Długość Działki Kielicha(cm)', 'Szerokość Działki Kielicha(cm)', 'Długość Płatka(cm)',
-                'Szerokość Płatka(cm)', 'Gatunek']
 
-treningowy = pd.read_csv('data_train.csv', names=nazwy_kolumn)
-testowy = pd.read_csv('data_test.csv', names=nazwy_kolumn)
+coll_name = ['Długość Działki Kielicha(cm)', 'Szerokość Działki Kielicha(cm)', 'Długość Płatka(cm)',
+             'Szerokość Płatka(cm)', 'Gatunek']
 
-lista_minimum_maksimum = []
-for i in range(treningowy.shape[1] - 1):
-    lista_minimum_maksimum.append(min(treningowy[nazwy_kolumn[i]]))
-    lista_minimum_maksimum.append(max(treningowy[nazwy_kolumn[i]]))
-lista_treningowa = [treningowy[nazwy_kolumn[0]].tolist(), treningowy[nazwy_kolumn[1]].tolist(),
-                    treningowy[nazwy_kolumn[2]].tolist(), treningowy[nazwy_kolumn[3]].tolist(),
-                    treningowy[nazwy_kolumn[4]].tolist()]
-lista_testowa = [testowy[nazwy_kolumn[0]].tolist(), testowy[nazwy_kolumn[1]].tolist(),
-                 testowy[nazwy_kolumn[2]].tolist(), testowy[nazwy_kolumn[3]].tolist(),
-                 testowy[nazwy_kolumn[4]].tolist()]
-for i in range(len(lista_treningowa) - 1):
-    for j in range(len(lista_treningowa[i])):
-        lista_treningowa[i][j] = knn.normalizacja(lista_minimum_maksimum[i * 2], lista_minimum_maksimum[i * 2 + 1],
-                                                  lista_treningowa[i][j])
-for i in range(len(lista_testowa) - 1):
-    for j in range(len(lista_testowa[i])):
-        lista_testowa[i][j] = knn.normalizacja(lista_minimum_maksimum[i * 2], lista_minimum_maksimum[i * 2 + 1],
-                                               lista_testowa[i][j])
+trainingSet = pd.read_csv('data_train.csv', names=coll_name)
+test = pd.read_csv('data_test.csv', names=coll_name)
 
-ile_gatunkow = knn.sprawdz_ile_gatunkow(lista_treningowa[-1])
-macierz_pomylek = []
+list_min_max = []
+for i in range(trainingSet.shape[1] - 1):
+    list_min_max.append(min(trainingSet[coll_name[i]]))
+    list_min_max.append(max(trainingSet[coll_name[i]]))
+train_list = [trainingSet[coll_name[0]].tolist(), trainingSet[coll_name[1]].tolist(),
+              trainingSet[coll_name[2]].tolist(), trainingSet[coll_name[3]].tolist(),
+              trainingSet[coll_name[4]].tolist()]
+test_list = [test[coll_name[0]].tolist(), test[coll_name[1]].tolist(),
+             test[coll_name[2]].tolist(), test[coll_name[3]].tolist(),
+             test[coll_name[4]].tolist()]
+for i in range(len(train_list) - 1):
+    for j in range(len(train_list[i])):
+        train_list[i][j] = knn.scaling(list_min_max[i * 2], list_min_max[i * 2 + 1],
+                                       train_list[i][j])
+for i in range(len(test_list) - 1):
+    for j in range(len(test_list[i])):
+        test_list[i][j] = knn.scaling(list_min_max[i * 2], list_min_max[i * 2 + 1],
+                                      test_list[i][j])
+
+species_number = knn.check_species_number(train_list[-1])
+error_matrix = []
 lista_pomylek_pomoc = []
-for i in range(ile_gatunkow):
+for i in range(species_number):
     lista_pomylek_pomoc.append(0)
-for i in range(ile_gatunkow):
-    macierz_pomylek.append(lista_pomylek_pomoc.copy())
+for i in range(species_number):
+    error_matrix.append(lista_pomylek_pomoc.copy())
 
-rysuj(True, 0, 0)
-rysuj(False, 0, 1)
-rysuj(False, 0, 2)
-rysuj(False, 0, 3)
-rysuj(False, 1, 2)
-rysuj(False, 1, 3)
-rysuj(False, 2, 3)
+draw(True, 0, 0, "Wszystkie 4 cechy jednocześnie")
 
+for attr1, attr2 in atributes_combinations:
+    draw(False, attr1, attr2, coll_name[attr1] + " i " + coll_name[attr2])
+# iterowanie po dwóch kolekcjach.
