68d6f422a8412e7a0a3bc163b96572f4a4c04137
247644 Dominiak Maciej <247644@edu.p.lodz.pl>
2024-01-23 12:12:23 +0100

    kjhjh

--- 3/knn.py
+++ 3/knn.py
@@ -39,13 +39,16 @@ def distance_for_four(x1, x2, y1, y2, z1, z2, v1, v2):
 def knn(list_of_distances, list_of_species, k):
     indexes_that_are_closest = []
     num_for_species = []
-    for i in range(check_species_number(list_of_species)): #wypełnienie listy ile jest możliwych gatunków
+    for i in range(check_species_number(list_of_species)):
+        #wypełnienie listy ile jest możliwych gatunków
         num_for_species.append(0)
-    for i in range(k):                            #wypełnienie tablicy tak, żeby było k elementów, działamy na indeksach, które mają odniesienie do listy odległości
+    for i in range(k):
+    #wypełnienie tablicy tak, żeby było k elementów, działamy na indeksach, które mają odniesienie do listy odległości
         indexes_that_are_closest.append(i)
     for i in range(k - 1):
         for j in range(k - i - 1):
-            if list_of_distances[indexes_that_are_closest[j]] > list_of_distances[   #sortowanie tablicy od najmniejszej wartości odległości
+            if list_of_distances[indexes_that_are_closest[j]] > list_of_distances[
+                #sortowanie tablicy od najmniejszej wartości odległości
                 indexes_that_are_closest[j + 1]]:
                 var = indexes_that_are_closest[j]
                 indexes_that_are_closest[j] = indexes_that_are_closest[j + 1]
@@ -55,7 +58,8 @@ def knn(list_of_distances, list_of_species, k):
             if list_of_distances[i] < list_of_distances[indexes_that_are_closest[j]]:
                 for l in range(k - 1, j, -1):
                     indexes_that_are_closest[l] = indexes_that_are_closest[l - 1]
-                indexes_that_are_closest[j] = i                                                    #sprawdzenie, czy badany element jest mniejszy od któregoś z elementów tablicy indeksów, jeśli tak to przesuwamy wszystkie elementy większe o jeden w prawo (oprócz ostatniego, który znika z tablicy) i wkładamy w odpiowiednie miejsce nr indeksowy tego elementu
+                indexes_that_are_closest[j] = i
+                #sprawdzenie, czy badany element jest mniejszy od któregoś z elementów tablicy indeksów, jeśli tak to przesuwamy wszystkie elementy większe o jeden w prawo (oprócz ostatniego, który znika z tablicy) i wkładamy w odpiowiednie miejsce nr indeksowy tego elementu
                 break
     for i in range(k):
         num_for_species[list_of_species[indexes_that_are_closest[i]]] += 1 #podliczamy, z jakiej klasy są elementy o najmniejszych odległościach
